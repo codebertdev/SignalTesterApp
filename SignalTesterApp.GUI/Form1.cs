8c1893713976824526b6b0eaed963e2a74acb906
Simon Róbert <simonrobertistvan@gmail.com>
2025-05-28 22:40:02 +0200

    minor fixes

--- SignalTesterApp.GUI/Form1.cs
+++ SignalTesterApp.GUI/Form1.cs
@@ -16,7 +16,7 @@ namespace SignalTesterApp.GUI
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            LoadData(); // vagy amit szeretnél indításkor
+            LoadData();
         }
 
         private void LoadData()
